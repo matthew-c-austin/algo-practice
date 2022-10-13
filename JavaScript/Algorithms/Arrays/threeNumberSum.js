246c316bab69d86f67cfb532cca3a70b55ff2fd1
Matthew Austin <maustin803@gmail.com>
2022-10-12 19:13:01 -0700

    Added three number sum

---
+++ JavaScript/Algorithms/Arrays/threeNumberSum.js
@@ -0,0 +1,85 @@
+/* Write a function that takes in a non-empty array of distinct integers and an 
+integer representing a target sum. The function should find all triplets in the 
+array that sum up to the target sum and return a two-dimensional array of all these 
+triplets. The numbers in each triplet should be ordered in ascending order, and the 
+triplets themselves should be ordered in ascending order with respect to the numbers 
+they hold.
+
+If no three numbers sum up to the target sum, the function should return an empty array.
+
+Sample Input
+array =  [12, 3, 1, 2, -6, 5, -8, 6]
+targetSum = 0
+
+Sample Outputs
+[[-8, 2, 6], [-8, 3, 5], [-6, 1, 5]]
+
+/* Solution 1
+We could sort the input array to traverse in order. With each index of the sorted array
+we can traverse the following numbers, setting a potentialMatch value to check. For every 
+match we increment the 2nd value and then check the remaining values for a potentialMatch.
+
+Or we can create a hash table that contains all values. As we traverse the sorted
+array for potential match we would then check the hash table for it. This would be a n^2 solution
+still, as the O(nlogn) of the sorting is dwarfed by large data sets. 
+
+O(n^2) time | O(n) space - where n is the length of the array */
+
+function threeNumberSumSolution1(array, targetSum) {
+
+    const nums = {};
+    array.sort((a, b) => a - b);
+    const triplets = [];
+
+    //Create a hash table with values of array
+    for (const num of array) {
+        nums[num] = true;
+    }
+
+    for (let i = 0; i < array.length - 2; i++) {
+        const num1 = array[i];
+        for (let j = i + 1; j < array.length - 1; j++) {
+            const num2 = array[j];
+            const potentialMatch = targetSum - num2 - num1;
+            if (potentialMatch in nums && num2 < potentialMatch) {
+                const match = [num1, num2, potentialMatch]
+                triplets.push(match);
+            }   
+        }
+
+    }
+    return triplets;
+}
+
+/* Solution 2
+Another option is to use pointers and a while loop without a hash table. This
+prevents the need for additional conditionals to prevent unordered and repeat triplets
+from being added to the triplets array.
+
+O(n^2) time | O(n) space - where n is the length of the array */
+
+function threeNumberSumSolution2(array, targetSum) {
+    array.sort((a, b) => a - b);
+    const triplets =[];
+  
+    for (let i = 0; i < array.length - 2; i++) {
+      let leftPointer = i + 1;
+      let rightPointer = array.length - 1;
+      while (leftPointer < rightPointer) {
+        const currentSum = array[i] + array[leftPointer] + array[rightPointer];
+        if (currentSum === targetSum) {
+          triplets.push([array[i], array[leftPointer], array[rightPointer]]);
+          leftPointer++;
+          rightPointer--;
+        } else if (currentSum < targetSum) {
+          leftPointer++;
+        } else if (currentSum > targetSum) {
+          rightPointer--;
+        }
+      }
+    }
+    return triplets;
+  }
+
+exports.threeNumberSumSolution1 = threeNumberSumSolution1;
+exports.threeNumberSumSolution2 = threeNumberSumSolution2;
---
+++ JavaScript/test/Arrays/threeNumberSum.test.js
@@ -0,0 +1,44 @@
+const program = require('../../Algorithms/Arrays/threeNumberSum');
+const expect = require('chai').expect;
+
+// Case #1: from sample
+it('Test Case #1', function () {
+    const array = [12, 3, 1, 2, -6, 5, -8, 6];
+    const targetSum = 0;
+    const expected = [
+        [-8, 2, 6],
+        [-8, 3, 5],
+        [-6, 1, 5],
+        ];
+    expect(program.threeNumberSumSolution1(array, targetSum)).to.deep.equal(expected);
+    expect(program.threeNumberSumSolution2(array, targetSum)).to.deep.equal(expected);
+});
+
+// Case #2: one triplet as input and output 
+it('Test Case #2', function () {
+    const array = [0, 1, 2];
+    const targetSum = 3;
+    const expected = [
+        [0, 1, 2],
+        ];
+    expect(program.threeNumberSumSolution1(array, targetSum)).to.deep.equal(expected);
+    expect(program.threeNumberSumSolution2(array, targetSum)).to.deep.equal(expected);
+});
+
+// Case #2: one triplet as input and empty array output (no possible triplets)
+it('Test Case #3', function () {
+    const array = [0, 1, 2];
+    const targetSum = 7;
+    const expected = [];
+    expect(program.threeNumberSumSolution1(array, targetSum)).to.deep.equal(expected);
+    expect(program.threeNumberSumSolution2(array, targetSum)).to.deep.equal(expected);
+});
+
+// Case #4: one triplet as input and targetSum equal to one index of input array
+it('Test Case #4', function () {
+    const array = [0, 1, 2];
+    const targetSum = 0;
+    const expected = [];
+    expect(program.threeNumberSumSolution1(array, targetSum)).to.deep.equal(expected);
+    expect(program.threeNumberSumSolution2(array, targetSum)).to.deep.equal(expected);
+});
